5472bfff170c287ca8f6c20d2a1233aa051a2578
Jess <meg22152@gmail.com>
2021-08-23 21:09:12 +0800

    Initial Commit

---
+++ Speak app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Speak app
+//
+//  Created by Jess Hsu on 2021/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
