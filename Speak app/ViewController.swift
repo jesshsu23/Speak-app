1019514f1440a542e3fcac3a8a1146c84fa86e89
Jess <meg22152@gmail.com>
2021-08-25 18:38:37 +0800

    speak app

--- Speak app/ViewController.swift
+++ Speak app/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+   //輸入文字
+    @IBOutlet weak var TextField: UITextField!
+    
+    //聲音
+
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    
+    @IBOutlet weak var rateSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //女朋友
+    func girlfriend(language: String){
+            let speechUtterance =  AVSpeechUtterance(string: TextField.text!)
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: language)
+            let synthesizer = AVSpeechSynthesizer()
+        speechUtterance.rate = rateSlider.value
+                        speechUtterance.volume = volumeSlider.value
+        synthesizer.speak(speechUtterance)
+        }
 
+    @IBAction func AmericanGirlfriend(_ sender: Any) {
+        girlfriend(language:"en-US" )
+    }
+    
+    @IBAction func JapanGirlfriend(_ sender: Any) {
+        girlfriend(language:"ja-JP" )
+    }
+    
+    @IBAction func TaiwanGrilfriend(_ sender: Any){ girlfriend(language:"zh-TW" )
+    }
+    
+   
+  
+    
 }
 
